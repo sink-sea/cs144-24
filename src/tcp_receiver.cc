74a202d49cb322edef340e9b23754e7fec97b193
iwaxi <iwaxi_dy@tju.edu.cn>
2024-12-10 14:21:40 +0800

    modified TCP receiver: added comments; made some changes.

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -3,6 +3,10 @@
 using namespace std;
 
 void TCPReceiver::receive(TCPSenderMessage message) {
+    if (writer().is_closed()) {
+        return;
+    }
+
     if (message.SYN) {
         zero_point_ = move(message.seqno);
         syn_ = true;
@@ -12,15 +16,21 @@ void TCPReceiver::receive(TCPSenderMessage message) {
         reader().set_error();
     }
 
-    reassembler_.insert(
-      message.seqno.unwrap(zero_point_ + !message.SYN, writer().bytes_pushed()), message.payload, message.FIN);
+    reassembler_.insert(message.seqno.unwrap(zero_point_ + !message.SYN, writer().bytes_pushed()),
+                        move(message.payload),
+                        message.FIN);
 }
 
 TCPReceiverMessage TCPReceiver::send() const {
     auto writer_ = this->writer();
+
+    /* abs_seqno = pushed bytes + SYN + FIN, wrap it */
     auto ackno_
       = syn_ ? optional {Wrap32::wrap(writer_.bytes_pushed() + syn_ + writer_.is_closed(), zero_point_)} : nullopt;
-    uint16_t wnd_size_
-      = writer_.available_capacity() > UINT16_MAX ? UINT16_MAX : (uint16_t)writer_.available_capacity();
+    
+    /* window size */
+    uint16_t wnd_size_ = writer_.available_capacity() > UINT16_MAX
+                           ? static_cast<uint16_t>(UINT16_MAX)
+                           : static_cast<uint16_t>(writer_.available_capacity());
     return {ackno_, wnd_size_, writer_.has_error()};
 }
